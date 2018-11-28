1046add76776d3da783263c25a467844b376046e
Esfahani <esfahanimichael@gmail.com>
2018-11-28 15:38:47 -0800

    finished updates

---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default props =>(
+    <div>
+        <h1 className="center">
+            Stuff about us
+        </h1>
+        <p>AAAAABBBBBBB</p>
+        <p>CCCCCDDDDD</p>
+    </div>
+)
