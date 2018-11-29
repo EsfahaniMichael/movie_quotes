305ee89c2c7093dab16994e9f481a818c8e531fc
Esfahani <esfahanimichael@gmail.com>
2018-11-28 18:09:15 -0800

    finishing updates

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import auth from '../hoc/auth';
 
+class Quotes extends Component {
 
-export default props =>(
-    <div>
-        <h1 className="center">
-            Movie quotes
-        </h1>
-        <h5>
-            VERY NICE, HOW MUCH??
-        </h5>
+    render() {
 
-    </div>
-)
+        return(
+            <div>
+                <h1 className="center">
+                    Movie quotes
+                </h1>
+                <h5>
+                    VERY NICE, HOW MUCH??
+                </h5>
+
+            </div>
+        )
+    }
+}
+
+export default auth(Quotes);
