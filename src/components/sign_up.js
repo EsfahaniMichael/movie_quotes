3f514a131b1d7640bd208e0eb882ee4da71af7bf
Esfahani <esfahanimichael@gmail.com>
2018-11-30 13:34:24 -0800

    updates completed

--- src/components/sign_up.js
+++ src/components/sign_up.js
@@ -1,15 +1,62 @@
+
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form'
+
+import Input from './input';
+
 
 class SignUp extends Component {
-   render(){
-    return (
+
+
+    handleSignUp = (values) => {
+        console.log('sign up', values);
+    }
+
+
+    render(){
+        const {handleSubmit} = this.props;
+        return(
             <div>
-                <h1 className="center">
-                    Sign Up
-                </h1>
+                <h1 className="center">Sign Up</h1>
+
+                <form onSubmit={handleSubmit(this.handleSignUp)}>
+                    <div className="row">
+                        <Field name="email" label="Email" component={Input}/>
+                    </div>
+                    <div className="row">
+                        <Field size="s6" type="password" name="password" label="Password" component={Input}/>
+                        <Field size="s6" type="password" name="confirmPassword" label="Confirm Password" component={Input}/>
+                    </div>
+                    <div className="row">
+                        <div className="col s12 right-align">
+                            <button className="btn red darken-2">Sign Up</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         )
-   }
+    }
 }
 
-export default SignUp;
+
+function validate(values){
+    const {confirmPassword, email, password} = values;
+    const errors = {};
+
+    if(!email) {
+        errors.email = 'Please enter your email';
+    }
+    if (!password){
+        errors.password = 'Please creat a password';
+    }
+    if(password !== confirmPassword){
+        errors.confirmPassword = 'Passwords do not match';
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    form: 'sign_up',
+    validate: validate
+})(SignUp);
